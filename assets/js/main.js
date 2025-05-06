52742d3bb15d919f1b777b6885f400a1f0d9cbbd
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-05-06 00:13:36 +0000

    modificando cosas

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+ 
+  /* alert("ayuda");
+  */
+
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  $("#btn").click(function(){
+    alert("Correo Enviado correctamente")
+});
